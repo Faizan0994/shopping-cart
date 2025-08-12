8133055dbed2cc533a7901a9ac548ad4dc34cb9c
Faizan <faizanamjad575@gmail.com>
2025-08-12 15:32:10 +0500

    add the home page to default route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,10 @@
 import App from "./App";
-const routes = [{ path: "/", element: <App /> }];
+import Home from "./components/Home";
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        children: [{ index: true, element: <Home /> }],
+    },
+];
 export default routes;
