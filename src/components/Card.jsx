b03b8e8ed14d9d5b406836f678712f2c8e64f9cc
Faizan <faizanamjad575@gmail.com>
2025-08-13 23:43:43 +0500

    create product card

---
+++ src/components/Card.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import styles from "../styles/card.module.css";
+
+function Desc({ text, maxLength = 40 }) {
+    // for the 'read more' functionality
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    if (text.length <= maxLength) {
+        return <p className={styles.desc}>{text}</p>;
+    }
+
+    const displayedText = isExpanded ? text : text.slice(0, maxLength) + "...";
+
+    return (
+        <p className={styles.desc}>
+            {displayedText}{" "}
+            <button onClick={() => setIsExpanded((prev) => !prev)}>
+                {isExpanded ? "read less" : "read more"}
+            </button>
+        </p>
+    );
+}
+
+function Card({ product, description = "", price, image }) {
+    const [quantity, setQuantity] = useState(1);
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.imgSection}>
+                <img src={image} alt="" />
+            </div>
+            <div className={styles.detailSection}>
+                <h2>{product}</h2>
+                <Desc text={description} />
+                <p className={styles.price}>$ {price}</p>
+                <div className={styles.actions}>
+                    <label htmlFor="quantityInput" className={styles.qtyLabel}>
+                        Quantity:
+                    </label>
+                    <div className={styles.inputContainer}>
+                        <button
+                            className={styles.qtyButton}
+                            onClick={() => setQuantity(quantity + 1)}
+                        >
+                            +
+                        </button>
+                        <input
+                            type="number"
+                            id="quantityInput"
+                            value={quantity}
+                        />
+                        <button
+                            className={styles.qtyButton}
+                            onClick={() => setQuantity(quantity - 1)}
+                        >
+                            -
+                        </button>
+                    </div>
+                    <button className={styles.buyButton}>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
