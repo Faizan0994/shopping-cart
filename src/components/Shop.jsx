74c84d31fa90099c229f187c4f050ff02b87b15f
Faizan <faizanamjad575@gmail.com>
2025-08-14 15:30:27 +0500

    render actual products

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/shop.module.css";
 import Card from "./Card";
 
 function Shop() {
-    return (
-        <div className={styles.shop}>
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-            <Card product={""} description="" price={""} image={""} />
-        </div>
-    );
+    const [products, setProducts] = useState(null);
+    useEffect(() => {
+        async function getProducts() {
+            const promise = await fetch("https://fakestoreapi.com/products");
+            const data = await promise.json();
+            setProducts(data);
+        }
+        getProducts();
+    }, []);
+
+    function generateCards() {
+        if (products)
+            return (
+                <>
+                    {products.map((item) => {
+                        return (
+                            <Card
+                                product={item.title}
+                                description={item.description}
+                                price={item.price}
+                                image={item.image}
+                                key={item.id}
+                            />
+                        );
+                    })}
+                </>
+            );
+        return <></>;
+    }
+    return <div className={styles.shop}>{generateCards()}</div>;
 }
 
 export default Shop;
