3307d846dab90df686f2ed2eb58f7365f31e09e3
Faizan <faizanamjad575@gmail.com>
2025-08-14 20:29:06 +0500

    make the shop button functional

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,22 @@
 import styles from "../styles/home.module.css";
 import bgImgage from "../assets/shopBackground.jpg";
+import { useNavigate } from "react-router-dom";
 function Home() {
+    const navigate = useNavigate(); // can't call the hook directly within the callback
+
     return (
         <div className={styles.home}>
             <img src={bgImgage} alt="" />
             <section>
                 <h1>Highest Quality Products</h1>
                 <p>Available at the best prices in the market</p>
-                <button>Shop Now</button>
+                <button
+                    onClick={() => {
+                        navigate("/shop");
+                    }}
+                >
+                    Shop Now
+                </button>
             </section>
         </div>
     );
